ec550aabb3961794f7e4ad6f6106d2ad1a19db84
kang <dkemfjqek@gmail.com>
2021-05-02 23:22:37 +0900

    project init

---
+++ src/main/java/com/singlePage/SinglePageApplication.java
@@ -0,0 +1,13 @@
+package com.singlePage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinglePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinglePageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singlePage/SinglePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singlePage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinglePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
