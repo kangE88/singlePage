090520492cc1a4f494132d4b143bc8bda9616230
berenzan <falazure@naver.com>
2021-05-04 00:27:40 +0900

    conrim TEST

--- src/main/resources/static/jsTest.js
+++ src/main/resources/static/jsTest.js
@@ -1,7 +1,29 @@
 const globFunc = {};
 
-globFunc.test = function() {
+globFunc.test = function (message) {
     debugger;
     let locVar = "loc";
-    alert("야발");
+
+    confirm('우리혁 화이팅이라고 외치시겠습니까?', function (ans) {
+        if (true == ans) {
+            globFunc.callTest();
+        } else {
+            globFunc.test();
+        }
+
+    });
+}
+
+globFunc.callTest = function() {
+    let info = {'first':true, 'second':'true'};
+    globFunc.funcTest(info);
+}
+
+globFunc.funcTest = function(info) {
+    let vTest = info.first;
+    if(true === vTest) {
+        console.log("this variable is varTest let");
+    }
+
+
 }
