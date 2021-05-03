79a812048e842ea88ea1b5cb327b8354a91cfadf
berenzan <falazure@naver.com>
2021-05-04 00:44:09 +0900

    test

--- src/main/resources/static/jsTest.js
+++ src/main/resources/static/jsTest.js
@@ -3,13 +3,14 @@ const globFunc = {};
 globFunc.test = function (message) {
     debugger;
     let locVar = "loc";
-    
+    let ans    = undefined;
+
     console.log("현재 테스트 환경은 : " + locVar);
-    
+
     if(prompt('우리혁 화이팅이라고 외치시겠습니까?(예,아니오)') === '예') {
-        let ans = confirm('globFunc.callTest를 실행합니다.');
+        ans = confirm('globFunc.callTest를 실행합니다.');
         if(true === ans) {
-            globFunc.callTest();
+            globFunc.callTest(ans);
 
         } else {
             alert('종료');
@@ -18,7 +19,8 @@ globFunc.test = function (message) {
 
     } else {
         alert('예 라고 쓰십시오 좆간.');
-        globFunc.test();
+        ans = 'true';
+        globFunc.test(ans);
 
     }
 };
@@ -26,20 +28,23 @@ globFunc.test = function (message) {
 /**
  * function call test
  */
-globFunc.callTest = function() {
-    let info = {'first':true, 'second':'true'};
+globFunc.callTest = function(ans) {
+    let info = {'first':ans, 'second':'두번째는없다.'};
     globFunc.jsonTest(info);
 
 }; // end globFunc.callTest = function()
 
 /**
  * json div Test
- * @param info  first:true, second:'true'
+ * @param info  first:ans, second:'^^'
  */
 globFunc.jsonTest = function(info) {
     let vTest = info.first;
     if(true === vTest) {
-        console.log("this variable is varTest let");
+        console.log("this variable is varTest let type of : " + typeof vTest);
 
+    } else {
+        console.log("this variable is varTest let type of : " + typeof vTest);
     }
+
 }; // end globFunc.jsonTest = function(info)
