fe7c7a2b845d6fb1369b2132b3a8165aa0ea9366
berenzan <falazure@naver.com>
2021-05-04 00:39:10 +0900

    conrim TEST

--- src/main/resources/static/jsTest.js
+++ src/main/resources/static/jsTest.js
@@ -3,27 +3,43 @@ const globFunc = {};
 globFunc.test = function (message) {
     debugger;
     let locVar = "loc";
-
-    confirm('우리혁 화이팅이라고 외치시겠습니까?', function (ans) {
-        if (true == ans) {
+    
+    console.log("현재 테스트 환경은 : " + locVar);
+    
+    if(prompt('우리혁 화이팅이라고 외치시겠습니까?(예,아니오)') === '예') {
+        let ans = confirm('globFunc.callTest를 실행합니다.');
+        if(true === ans) {
             globFunc.callTest();
+
         } else {
-            globFunc.test();
+            alert('종료');
+
         }
 
-    });
-}
+    } else {
+        alert('예 라고 쓰십시오 좆간.');
+        globFunc.test();
 
+    }
+};
+
+/**
+ * function call test
+ */
 globFunc.callTest = function() {
     let info = {'first':true, 'second':'true'};
-    globFunc.funcTest(info);
-}
+    globFunc.jsonTest(info);
 
-globFunc.funcTest = function(info) {
+}; // end globFunc.callTest = function()
+
+/**
+ * json div Test
+ * @param info  first:true, second:'true'
+ */
+globFunc.jsonTest = function(info) {
     let vTest = info.first;
     if(true === vTest) {
         console.log("this variable is varTest let");
+
     }
-
-
-}
+}; // end globFunc.jsonTest = function(info)
