ed436c5dbf19a9d4e1833b994c1ab9ceba0f9a59
kang <dkemfjqek@gmail.com>
2021-05-07 11:20:21 +0900

    h2 database -> Mongo DB로 변경 접속까진 이상없는듯..?

--- src/main/java/com/security/SecurityController.java
+++ src/main/java/com/security/SecurityController.java
@@ -1,12 +1,35 @@
 package com.security;
 
+import com.repo.UserRepository;
+import com.user.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SecurityController {
+public class SecurityController implements CommandLineRunner {
+
+    @Autowired
+    UserRepository repository;
+
     @GetMapping("/")
     public String index(){
+        System.out.println("home in");
         return "home";
+    }
+
+    public void run(String... args) throws Exception {
+        deleteAll();
+        addSampleData();
+    }
+    public void deleteAll() {
+        System.out.println("Deleting all records..");
+        repository.deleteAll();
+    }
+
+    public void addSampleData() {
+        System.out.println("Adding sample data");
+        repository.save(new Users("kang", "kang", "kang88"));
     }
 }
